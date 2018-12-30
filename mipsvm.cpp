49aab997f224e7d8c99e711222c0720c0a2f4816
Hans-Kristian Arntzen <maister@archlinux.us>
2018-12-30 13:45:37 +0100

    Add unaligned load instructions.

--- mipsvm.cpp
+++ mipsvm.cpp
@@ -417,10 +417,6 @@ enum class Op
 	ORI,
 	XORI,
 	LUI,
-	COP0,
-	COP1,
-	COP2,
-	COP3,
 	LB,
 	LH,
 	LWL,
@@ -433,6 +429,8 @@ enum class Op
 	SWL,
 	SW,
 	SWR,
+
+#if 0
 	LWC0,
 	LWC1,
 	LWC2,
@@ -441,6 +439,11 @@ enum class Op
 	SWC1,
 	SWC2,
 	SWC3,
+	COP0,
+	COP1,
+	COP2,
+	COP3,
+#endif
 };
 
 struct MIPSInstruction
@@ -777,6 +780,11 @@ public:
 
 	ExitState enter(Address addr) noexcept;
 
+	uint32_t lwl(Address addr, uint32_t old_value) const noexcept;
+	uint32_t lwr(Address addr, uint32_t old_value) const noexcept;
+	void swl(Address addr, uint32_t value) noexcept;
+	void swr(Address addr, uint32_t value) noexcept;
+
 	void store32(Address addr, uint32_t value) noexcept;
 	void store16(Address addr, uint32_t value) noexcept;
 	void store8(Address addr, uint32_t value) noexcept;
@@ -824,8 +832,17 @@ private:
 	void create_break(Recompiler *recompiler, Value *argument, BasicBlock *bb, Address addr);
 	void create_syscall(Recompiler *recompiler, Value *argument, BasicBlock *bb, Address addr);
 
+	Value *create_lwl(Recompiler *recompiler, Value *argument, BasicBlock *bb, Value *old_value, Value *addr);
+	Value *create_lwr(Recompiler *recompiler, Value *argument, BasicBlock *bb, Value *old_value, Value *addr);
+	void create_swl(Recompiler *recompiler, Value *argument, BasicBlock *bb, Value *addr, Value *value);
+	void create_swr(Recompiler *recompiler, Value *argument, BasicBlock *bb, Value *addr, Value *value);
+
 	struct
 	{
+		llvm::Function *lwl = nullptr;
+		llvm::Function *lwr = nullptr;
+		llvm::Function *swl = nullptr;
+		llvm::Function *swr = nullptr;
 		llvm::Function *store32 = nullptr;
 		llvm::Function *store16 = nullptr;
 		llvm::Function *store8 = nullptr;
@@ -902,6 +919,26 @@ static void backend_syscall(RegisterState *regs, Address addr)
 {
 	static_cast<MIPS *>(regs)->op_syscall(addr);
 }
+
+static uint32_t backend_lwl(RegisterState *regs, Address addr, uint32_t old_value)
+{
+	return static_cast<MIPS *>(regs)->lwl(addr, old_value);
+}
+
+static uint32_t backend_lwr(RegisterState *regs, Address addr, uint32_t old_value)
+{
+	return static_cast<MIPS *>(regs)->lwr(addr, old_value);
+}
+
+static void backend_swl(RegisterState *regs, Address addr, uint32_t value)
+{
+	static_cast<MIPS *>(regs)->swl(addr, value);
+}
+
+static void backend_swr(RegisterState *regs, Address addr, uint32_t value)
+{
+	static_cast<MIPS *>(regs)->swr(addr, value);
+}
 }
 
 MIPS::MIPS()
@@ -917,6 +954,10 @@ MIPS::MIPS()
 	jitter.add_external_symbol("__recompiler_sigill", backend_sigill);
 	jitter.add_external_symbol("__recompiler_break", backend_break);
 	jitter.add_external_symbol("__recompiler_syscall", backend_syscall);
+	jitter.add_external_symbol("__recompiler_lwl", backend_lwl);
+	jitter.add_external_symbol("__recompiler_lwr", backend_lwr);
+	jitter.add_external_symbol("__recompiler_swl", backend_swl);
+	jitter.add_external_symbol("__recompiler_swr", backend_swr);
 
 	syscall_table[SYSCALL_EXIT] = &MIPS::syscall_exit;
 	syscall_table[SYSCALL_WRITE] = &MIPS::syscall_write;
@@ -954,26 +995,113 @@ void MIPS::store16(Address addr, uint32_t value) noexcept
 
 void MIPS::store8(Address addr, uint32_t value) noexcept
 {
-	auto *ptr = static_cast<uint16_t *>(addr_space.get_page(addr / VirtualAddressSpace::PageSize));
+	auto *ptr = static_cast<uint8_t *>(addr_space.get_page(addr / VirtualAddressSpace::PageSize));
 	ptr[(addr & (VirtualAddressSpace::PageSize - 1)) >> 0] = uint8_t(value);
+}
+
+uint32_t MIPS::lwl(Address addr, uint32_t old_value) const noexcept
+{
+	// Little-endian MIPS. Needs a different implementation for Big-endian.
+
+	uint32_t loaded = load32(addr);
+	uint32_t addr_offset = addr & 3;
+	loaded <<= (addr_offset ^ 3) * 8;
+	uint32_t retain_mask = (1u << 24) - 1u;
+	retain_mask >>= addr_offset * 8;
+	return (old_value & retain_mask) | (loaded & ~retain_mask);
+}
+
+uint32_t MIPS::lwr(Address addr, uint32_t old_value) const noexcept
+{
+	// Little-endian MIPS. Needs a different implementation for Big-endian.
+
+	uint32_t addr_offset = addr & 3;
+
+	uint32_t loaded = load32(addr);
+	loaded >>= addr_offset * 8;
+
+	uint32_t keep_mask = 0xffffffffu;
+	keep_mask >>= addr_offset * 8;
+	return (old_value & ~keep_mask) | (loaded & keep_mask);
+}
+
+void MIPS::swl(Address addr, uint32_t value) noexcept
+{
+	// Little-endian MIPS. Needs a different implementation for Big-endian.
+
+	uint32_t base_addr = addr & ~3;
+	uint32_t addr_offset = addr & 3;
+
+	switch (addr_offset)
+	{
+	case 0:
+		store8(base_addr, value >> 24);
+		break;
+
+	case 1:
+		store16(base_addr, value >> 16);
+		break;
+
+	case 2:
+		store8(base_addr + 0, uint8_t(value >> 8));
+		store8(base_addr + 1, uint8_t(value >> 16));
+		store8(base_addr + 2, uint8_t(value >> 24));
+		break;
+
+	case 3:
+		store32(base_addr, value);
+		break;
+	}
+}
+
+void MIPS::swr(Address addr, uint32_t value) noexcept
+{
+	// Little-endian MIPS. Needs a different implementation for Big-endian.
+
+	uint32_t base_addr = addr & ~3;
+	uint32_t addr_offset = addr & 3;
+
+	switch (addr_offset)
+	{
+	case 0:
+		store32(base_addr, value);
+		break;
+
+	case 1:
+		store8(base_addr + 1, uint8_t(value >> 0));
+		store8(base_addr + 2, uint8_t(value >> 8));
+		store8(base_addr + 3, uint8_t(value >> 16));
+		break;
+
+	case 2:
+		store16(base_addr + 2, uint16_t(value));
+		break;
+
+	case 3:
+		store8(base_addr + 3, uint8_t(value));
+		break;
+	}
 }
 
 uint32_t MIPS::load32(Address addr) const noexcept
 {
 	auto *ptr = static_cast<uint32_t *>(addr_space.get_page(addr / VirtualAddressSpace::PageSize));
-	return ptr[(addr & (VirtualAddressSpace::PageSize - 1)) >> 2];
+	uint32_t loaded = ptr[(addr & (VirtualAddressSpace::PageSize - 1)) >> 2];
+	return loaded;
 }
 
 uint16_t MIPS::load16(Address addr) const noexcept
 {
 	auto *ptr = static_cast<uint16_t *>(addr_space.get_page(addr / VirtualAddressSpace::PageSize));
-	return ptr[(addr & (VirtualAddressSpace::PageSize - 1)) >> 1];
+	uint16_t loaded = ptr[(addr & (VirtualAddressSpace::PageSize - 1)) >> 1];
+	return loaded;
 }
 
 uint8_t MIPS::load8(Address addr) const noexcept
 {
 	auto *ptr = static_cast<uint8_t *>(addr_space.get_page(addr / VirtualAddressSpace::PageSize));
-	return ptr[(addr & (VirtualAddressSpace::PageSize - 1)) >> 0];
+	uint8_t loaded = ptr[(addr & (VirtualAddressSpace::PageSize - 1)) >> 0];
+	return loaded;
 }
 
 void MIPS::sigill(Address addr) const noexcept
@@ -1171,10 +1299,10 @@ void MIPS::get_block_from_address(Address addr, Block &block)
 
 		if (end_of_basic_block)
 		{
-			if (!mips_opcode_is_branch(instruction.op) || mips_opcode_is_branch(load_instr(addr + 4).op))
-				block.block_end = addr + 4;
-			else
+			if (mips_opcode_is_branch(instruction.op) && !mips_opcode_is_branch(load_instr(addr + 4).op))
 				block.block_end = addr + 8;
+			else
+				block.block_end = addr + 4;
 
 			switch (instruction.op)
 			{
@@ -1697,6 +1825,42 @@ void MIPS::recompile_instruction(Recompiler *recompiler, BasicBlock *&bb,
 		break;
 	}
 
+	case Op::LWL:
+	{
+		auto *loaded = create_lwl(recompiler, tracker.get_argument(), bb, tracker.read_int(instr.rt),
+		                          builder.CreateAdd(tracker.read_int(instr.rs),
+		                                            ConstantInt::get(Type::getInt32Ty(ctx), int16_t(instr.imm)), "LWLAddr"));
+		tracker.write_int(instr.rt, loaded);
+		break;
+	}
+
+	case Op::LWR:
+	{
+		auto *loaded = create_lwr(recompiler, tracker.get_argument(), bb, tracker.read_int(instr.rt),
+		                          builder.CreateAdd(tracker.read_int(instr.rs),
+		                                            ConstantInt::get(Type::getInt32Ty(ctx), int16_t(instr.imm)), "LWRAddr"));
+		tracker.write_int(instr.rt, loaded);
+		break;
+	}
+
+	case Op::SWL:
+	{
+		create_swl(recompiler, tracker.get_argument(), bb,
+		           builder.CreateAdd(tracker.read_int(instr.rs),
+		                             ConstantInt::get(Type::getInt32Ty(ctx), int16_t(instr.imm)), "SWLAddr"),
+		           tracker.read_int(instr.rt));
+		break;
+	}
+
+	case Op::SWR:
+	{
+		create_swr(recompiler, tracker.get_argument(), bb,
+		           builder.CreateAdd(tracker.read_int(instr.rs),
+		                             ConstantInt::get(Type::getInt32Ty(ctx), int16_t(instr.imm)), "SWRAddr"),
+		           tracker.read_int(instr.rt));
+		break;
+	}
+
 	default:
 		tracker.flush();
 		create_sigill(recompiler, tracker.get_argument(), bb, addr);
@@ -1797,6 +1961,40 @@ void MIPS::create_store32(Recompiler *recompiler, Value *argument, BasicBlock *b
 	builder.CreateCall(calls.store32, values);
 }
 
+void MIPS::create_swl(Recompiler *recompiler, Value *argument, BasicBlock *bb, Value *addr, Value *value)
+{
+	IRBuilder<> builder(bb);
+	auto &ctx = builder.getContext();
+
+	if (!calls.swl)
+	{
+		Type *store_types[] = { Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx), Type::getInt32Ty(ctx) };
+		auto *store_type = FunctionType::get(Type::getVoidTy(ctx), store_types, false);
+		calls.swl = llvm::Function::Create(store_type, llvm::Function::ExternalLinkage,
+		                                   "__recompiler_swl", recompiler->get_current_module());
+	}
+
+	Value *values[] = { argument, addr, value };
+	builder.CreateCall(calls.swl, values);
+}
+
+void MIPS::create_swr(Recompiler *recompiler, Value *argument, BasicBlock *bb, Value *addr, Value *value)
+{
+	IRBuilder<> builder(bb);
+	auto &ctx = builder.getContext();
+
+	if (!calls.swr)
+	{
+		Type *store_types[] = { Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx), Type::getInt32Ty(ctx) };
+		auto *store_type = FunctionType::get(Type::getVoidTy(ctx), store_types, false);
+		calls.swr = llvm::Function::Create(store_type, llvm::Function::ExternalLinkage,
+		                                   "__recompiler_swr", recompiler->get_current_module());
+	}
+
+	Value *values[] = { argument, addr, value };
+	builder.CreateCall(calls.swr, values);
+}
+
 void MIPS::create_store16(Recompiler *recompiler, Value *argument, BasicBlock *bb, Value *addr, Value *value)
 {
 	IRBuilder<> builder(bb);
@@ -1831,6 +2029,40 @@ void MIPS::create_store8(Recompiler *recompiler, Value *argument, BasicBlock *bb
 	builder.CreateCall(calls.store8, values);
 }
 
+Value *MIPS::create_lwl(Recompiler *recompiler, Value *argument, BasicBlock *bb, Value *old_value, Value *addr)
+{
+	IRBuilder<> builder(bb);
+	auto &ctx = builder.getContext();
+
+	if (!calls.lwl)
+	{
+		Type *load_types[] = { Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx), Type::getInt32Ty(ctx) };
+		auto *load_type = FunctionType::get(Type::getInt32Ty(ctx), load_types, false);
+		calls.lwl = llvm::Function::Create(load_type, llvm::Function::ExternalLinkage,
+		                                   "__recompiler_lwl", recompiler->get_current_module());
+	}
+
+	Value *values[] = { argument, addr, old_value };
+	return builder.CreateCall(calls.lwl, values);
+}
+
+Value *MIPS::create_lwr(Recompiler *recompiler, Value *argument, BasicBlock *bb, Value *old_value, Value *addr)
+{
+	IRBuilder<> builder(bb);
+	auto &ctx = builder.getContext();
+
+	if (!calls.lwr)
+	{
+		Type *load_types[] = { Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx), Type::getInt32Ty(ctx) };
+		auto *load_type = FunctionType::get(Type::getInt32Ty(ctx), load_types, false);
+		calls.lwr = llvm::Function::Create(load_type, llvm::Function::ExternalLinkage,
+		                                   "__recompiler_lwr", recompiler->get_current_module());
+	}
+
+	Value *values[] = { argument, addr, old_value };
+	return builder.CreateCall(calls.lwr, values);
+}
+
 Value *MIPS::create_load32(Recompiler *recompiler, Value *argument, BasicBlock *bb, Value *addr)
 {
 	IRBuilder<> builder(bb);
@@ -1838,7 +2070,7 @@ Value *MIPS::create_load32(Recompiler *recompiler, Value *argument, BasicBlock *
 
 	if (!calls.load32)
 	{
-		Type *load_types[] = {Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx)};
+		Type *load_types[] = { Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx) };
 		auto *load_type = FunctionType::get(Type::getInt32Ty(ctx), load_types, false);
 		calls.load32 = llvm::Function::Create(load_type, llvm::Function::ExternalLinkage,
 		                                      "__recompiler_load32", recompiler->get_current_module());
@@ -1855,7 +2087,7 @@ Value *MIPS::create_load16(Recompiler *recompiler, Value *argument, BasicBlock *
 
 	if (!calls.load16)
 	{
-		Type *load_types[] = {Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx)};
+		Type *load_types[] = { Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx) };
 		auto *load_type = FunctionType::get(Type::getInt16Ty(ctx), load_types, false);
 		calls.load16 = llvm::Function::Create(load_type, llvm::Function::ExternalLinkage,
 		                                      "__recompiler_load16", recompiler->get_current_module());
@@ -1872,7 +2104,7 @@ Value *MIPS::create_load8(Recompiler *recompiler, Value *argument, BasicBlock *b
 
 	if (!calls.load8)
 	{
-		Type *load_types[] = {Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx)};
+		Type *load_types[] = { Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx) };
 		auto *load_type = FunctionType::get(Type::getInt8Ty(ctx), load_types, false);
 		calls.load8 = llvm::Function::Create(load_type, llvm::Function::ExternalLinkage,
 		                                     "__recompiler_load8", recompiler->get_current_module());
@@ -1889,7 +2121,7 @@ void MIPS::create_sigill(Recompiler *recompiler, Value *argument, BasicBlock *bb
 
 	if (!calls.sigill)
 	{
-		Type *load_types[] = {Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx)};
+		Type *load_types[] = { Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx) };
 		auto *load_type = FunctionType::get(Type::getVoidTy(ctx), load_types, false);
 		calls.sigill = llvm::Function::Create(load_type, llvm::Function::ExternalLinkage,
 		                                      "__recompiler_sigill", recompiler->get_current_module());
@@ -1906,7 +2138,7 @@ void MIPS::create_break(Recompiler *recompiler, Value *argument, BasicBlock *bb,
 
 	if (!calls.op_break)
 	{
-		Type *load_types[] = {Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx)};
+		Type *load_types[] = { Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx) };
 		auto *load_type = FunctionType::get(Type::getVoidTy(ctx), load_types, false);
 		calls.op_break = llvm::Function::Create(load_type, llvm::Function::ExternalLinkage,
 		                                        "__recompiler_break", recompiler->get_current_module());
@@ -1923,7 +2155,7 @@ void MIPS::create_syscall(Recompiler *recompiler, Value *argument, BasicBlock *b
 
 	if (!calls.op_syscall)
 	{
-		Type *load_types[] = {Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx)};
+		Type *load_types[] = { Type::getInt32PtrTy(ctx), Type::getInt32Ty(ctx) };
 		auto *load_type = FunctionType::get(Type::getVoidTy(ctx), load_types, false);
 		calls.op_syscall = llvm::Function::Create(load_type, llvm::Function::ExternalLinkage,
 		                                          "__recompiler_syscall", recompiler->get_current_module());
@@ -1966,6 +2198,29 @@ int main(int argc, char **argv)
 	std::unordered_map<std::string, Address> symbol_table;
 	if (!load_elf(argv[1], ehdr, mips.get_address_space(), symbol_table))
 		return 1;
+
+	auto stack = symbol_table.find("__stack")->second;
+	for (unsigned i = 0; i < 16; i++)
+		mips.store8(stack + i, i);
+
+	call_int(mips, symbol_table, "test_ulw_emu", stack, 0, [](int32_t v0, int32_t) { assert_equal(v0, (0 << 0) | (1 << 8) | (2 << 16) | (3 << 24)); });
+	call_int(mips, symbol_table, "test_ulw_emu", stack, 1, [](int32_t v0, int32_t) { assert_equal(v0, (1 << 0) | (2 << 8) | (3 << 16) | (4 << 24)); });
+	call_int(mips, symbol_table, "test_ulw_emu", stack, 2, [](int32_t v0, int32_t) { assert_equal(v0, (2 << 0) | (3 << 8) | (4 << 16) | (5 << 24)); });
+	call_int(mips, symbol_table, "test_ulw_emu", stack, 3, [](int32_t v0, int32_t) { assert_equal(v0, (3 << 0) | (4 << 8) | (5 << 16) | (6 << 24)); });
+
+	call_int(mips, symbol_table, "test_ulw", stack, 0, [](int32_t v0, int32_t) { assert_equal(v0, (0 << 0) | (1 << 8) | (2 << 16) | (3 << 24)); });
+	call_int(mips, symbol_table, "test_ulw", stack, 1, [](int32_t v0, int32_t) { assert_equal(v0, (1 << 0) | (2 << 8) | (3 << 16) | (4 << 24)); });
+	call_int(mips, symbol_table, "test_ulw", stack, 2, [](int32_t v0, int32_t) { assert_equal(v0, (2 << 0) | (3 << 8) | (4 << 16) | (5 << 24)); });
+	call_int(mips, symbol_table, "test_ulw", stack, 3, [](int32_t v0, int32_t) { assert_equal(v0, (3 << 0) | (4 << 8) | (5 << 16) | (6 << 24)); });
+
+	call_int(mips, symbol_table, "test_usw", stack, 0xabcd1234, [](int32_t, int32_t) {});
+	call_int(mips, symbol_table, "test_ulw", stack, 0, [](uint32_t v0, int32_t) { assert_equal(v0, 0xabcd1234ll); });
+	call_int(mips, symbol_table, "test_usw", stack + 1, 0xabcd1234, [](int32_t, int32_t) {});
+	call_int(mips, symbol_table, "test_ulw", stack, 1, [](uint32_t v0, int32_t) { assert_equal(v0, 0xabcd1234ll); });
+	call_int(mips, symbol_table, "test_usw", stack + 2, 0xabcd1234, [](int32_t, int32_t) {});
+	call_int(mips, symbol_table, "test_ulw", stack, 2, [](uint32_t v0, int32_t) { assert_equal(v0, 0xabcd1234ll); });
+	call_int(mips, symbol_table, "test_usw", stack + 3, 0xabcd1234, [](int32_t, int32_t) {});
+	call_int(mips, symbol_table, "test_ulw", stack, 3, [](uint32_t v0, int32_t) { assert_equal(v0, 0xabcd1234ll); });
 
 	call_int(mips, symbol_table, "test_div", 2, 4, [](int32_t v0, int32_t v1) { assert_equal(v0, 0); assert_equal(v1, 2); });
 	call_int(mips, symbol_table, "test_div", -7, 3, [](int32_t v0, int32_t v1) { assert_equal(v0, -7 / 3); assert_equal(v1, -7 % 3); });
